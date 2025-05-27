8aa63b51d89a779501b7c5c07768d55609eb70fa
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2025-05-27 23:47:30 +0300

    add some simple types of variables

--- lesson1.dart
+++ lesson1.dart
@@ -1,3 +1,12 @@
-void main(){
-  print("Hello, Dart!");
+void main() {
+  String title = "Hello, Dart!";
+  int number = 43;
+  number = number ~/ 3;
+  var isActive = true;
+  bool isComplete = false;
+  title = title.toUpperCase();
+  const double pi = 3.141592653589793;
+  final String greeting = "Welcome to Dart programming!";
+  final date = DateTime.now();
+  print('$title $number $isActive $date');
 }
