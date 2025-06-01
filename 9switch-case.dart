624eadc2f476aea42bbed11cdfa431c913b091e5
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2025-06-01 11:27:51 +0300

    add lesson "switch-case"

---
+++ 9switch-case.dart
@@ -0,0 +1,17 @@
+void main() {
+  const student = 'John';
+
+  switch (student) {
+    case 'John':
+      print('John is a student');
+      break;
+    case 'Diana':
+      print('Diana is a student');
+      break;
+    case 'Mike':
+      print('Mike is a student');
+      break;
+    default:
+      print('Unknown student');
+  }
+}
