326489a2f8468911f55095c322604ac86a21b77c
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2025-06-02 09:23:57 +0300

    add printing of avriables

--- 1baseTypesOfVariables.dart
+++ 1baseTypesOfVariables.dart
@@ -10,5 +10,7 @@ void main() {
   const double pi = 3.141592653589793;
   final String greeting = "Welcome to Dart programming!";
   final date = DateTime.now();
-  print('$title $number $isActive $date $dynamicValue');
+  print(
+    '$title $number $isActive $date $dynamicValue $isComplete $pi $greeting',
+  );
 }
