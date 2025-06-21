4440730ef014424f2983fcdcf8e0bd5b02d70b87
Evgeniy Ivanchikay <ev.ivanchikov@gmail.com>
2025-06-21 10:41:22 +0300

    add constant constructor

---
+++ 29constant-constructors.dart
@@ -0,0 +1,21 @@
+void main() {
+  const alex = Human(name: 'Alex', age: 22, height: 184);
+
+  print(alex.getNameAndAge);
+}
+
+class Human {
+  final String name;
+  final int _age;
+  final num height;
+
+  const Human({required this.name, required int age, required this.height})
+    : _age = age;
+
+  int get age => _age;
+  String get getNameAndAge => '$name, $_age years old';
+
+  // void grewUp() {
+  //   this._age += 1;
+  // }
+}
